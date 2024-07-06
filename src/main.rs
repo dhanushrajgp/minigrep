0a888d3f6099bdd0c457244873083e924520abd4
dhanushrajgp <gdhanush68@gmail.com>
2024-07-06 11:46:03 +0530

    saving the argument values in variables

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,12 @@ use std::env;
 fn main() {
     println!("Hello, world!");
     let args: Vec<String> = env::args().collect();
+
+    let query = &args[0];
+    let file_path = &args[1];
+
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
+
     dbg!(args);
 }
