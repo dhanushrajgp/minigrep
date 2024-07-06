c3f8dd34af3069909c3fe9b43d8a7af1d27c4a45
dhanushrajgp <gdhanush68@gmail.com>
2024-07-06 13:38:21 +0530

    added test case for search and implemented search functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
 use std::{error::Error, fs};
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let content = fs::read_to_string(config.file_path)?;
-    println!("Content:\n {}", content);
+    let _content = fs::read_to_string(config.file_path)?;
     Ok(())
 }
 
@@ -19,5 +18,32 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
         Ok(Config { query, file_path })
+    }
+}
+
+pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in content.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let content = "\
+Rust:
+safe,fast,productive.
+pick three.";
+
+        assert_eq!(vec!["safe,fast,productive."], search(query, content))
     }
 }
