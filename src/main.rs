830ffbbf4029938cafbb976b7531112ddd553215
dhanushrajgp <gdhanush68@gmail.com>
2024-07-06 11:48:07 +0530

    fix/saving search and filepath variables from args

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,9 @@ fn main() {
     println!("Hello, world!");
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[0];
-    let file_path = &args[1];
+    let query = &args[1];
+    let file_path = &args[2];
 
     println!("Searching for {}", query);
     println!("In file {}", file_path);
-
-    dbg!(args);
 }
