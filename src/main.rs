4caa0d57e359877cb306d792ff5e160a4b0f7393
dhanushrajgp <gdhanush68@gmail.com>
2024-07-06 12:22:37 +0530

    created constructor for config to parse args maintaining standards of rust

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     println!("Hello, world!");
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -20,8 +20,10 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-    Config { query, file_path }
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Config { query, file_path }
+    }
 }
