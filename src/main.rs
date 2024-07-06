b7cc44a3855553595e2f957e3ab480064a024a91
dhanushrajgp <gdhanush68@gmail.com>
2024-07-06 12:04:32 +0530

    creating a seperate function for args parse logic

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,17 @@ fn main() {
     println!("Hello, world!");
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let (query, file_path) = parse_config(&args);
 
     println!("Searching for {}", query);
     println!("In file {}", file_path);
 
     let content = fs::read_to_string(file_path).expect("should have been able to read the file");
     println!("Content:\n {}", content);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+    (query, file_path)
 }
