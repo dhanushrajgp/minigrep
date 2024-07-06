1090ccda9e3ed6a1f7ed00b0439186c4aff3e524
dhanushrajgp <gdhanush68@gmail.com>
2024-07-06 12:14:11 +0530

    grouped configuration values as a Config struct

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,23 @@ fn main() {
     println!("Hello, world!");
     let args: Vec<String> = env::args().collect();
 
-    let (query, file_path) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let content = fs::read_to_string(file_path).expect("should have been able to read the file");
+    let content =
+        fs::read_to_string(config.file_path).expect("should have been able to read the file");
     println!("Content:\n {}", content);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
-    (query, file_path)
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+    Config { query, file_path }
 }
